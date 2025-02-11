253a7a75ae0963795b1c7fda746d313633db6ca0
Kijoung Song <kys21207@gmail.com>
2025-02-11 08:41:45 -0500

    Create filter_eqtl_sumstat.sh

---
+++ coloc_eqtl_gene_based/filter_eqtl_sumstat.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Ensure both input files are provided
+if [ $# -eq 0  ]; then
+    echo "Usage: $0 significant_ld_block.txt eqtl.all.tsv.gz eqtl"
+    exit 1
+fi
+
+SIGNIFICANT_LD_BLOCK=$1
+EQTL_FILE=$2
+eqtl_name_prefix=$3  
+OUTPUT_FILE="tmp_input/${eqtl_name_prefix}_subset.tsv.gz"
+
+# Create a temporary directory for intermediate files
+#TMP_DIR=$(mktemp -d)
+TMP_DIR=tmp
+mkdir -p $TMP_DIR
+
+# Create output directory if it doesn't exist
+mkdir -p "$(dirname "$OUTPUT_FILE")"
+
+# Convert significant_ld_block.txt to BED format and sort it
+awk 'BEGIN {OFS="\t"} {print substr($1, 4), $2, $3, $0}' $SIGNIFICANT_LD_BLOCK | sort -k1,1 -k2,2n > $TMP_DIR/significant_ld_block.bed
+
+# Extract the header from the eQTL file
+#zcat $EQTL_FILE | head -n 1 > $TMP_DIR/header
+
+# Add the header to the temporary directory
+echo -e "ensgid\tchr\tpos\tref\talt\tmaf\tbeta\tse" > $TMP_DIR/header
+
+# Convert the eQTL file to BED format
+#zcat $EQTL_FILE | tail -n +2 | awk 'BEGIN {OFS="\t"} {print $2, $3, $3, $0}' > $TMP_DIR/eqtl.bed
+
+# Use bedtools to intersect the BED files and extract relevant lines
+bedtools intersect -a $EQTL_FILE -b $TMP_DIR/significant_ld_block.bed -wa | cut -f4- > $TMP_DIR/filtered_eqtl
+
+# Combine the header and the filtered eQTL data
+cat $TMP_DIR/header $TMP_DIR/filtered_eqtl | gzip > $OUTPUT_FILE
+
+# Clean up temporary files
+#rm -r $TMP_DIR
+
+echo "Subset data has been saved to $OUTPUT_FILE"
